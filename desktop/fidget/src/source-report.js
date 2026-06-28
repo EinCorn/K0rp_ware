fcc08ea36853e4b4d874b344605545d3c29949e7
EinCorn <danielvallentino@gmail.com>
2026-06-28 22:56:03 +0200

    Count every Fidget-local pointer click

--- desktop/fidget/src/source-report.js
+++ desktop/fidget/src/source-report.js
@@ -1,17 +1,7 @@
 const REPORT_URL = 'http://127.0.0.1:47891/app-click?source=fidget'
-let pending = false
 
 function reportFidgetClick() {
-  if (pending) return
-
-  pending = true
-  window.queueMicrotask(() => {
-    fetch(REPORT_URL, { method: 'POST' })
-      .catch(() => {})
-      .finally(() => {
-        pending = false
-      })
-  })
+  fetch(REPORT_URL, { method: 'POST' }).catch(() => {})
 }
 
 document.addEventListener('pointerdown', reportFidgetClick, { capture: true })
