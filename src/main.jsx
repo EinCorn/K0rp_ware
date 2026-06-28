e29c5acafa16e87b19eb380152a73a3d886355a6
EinCorn <danielvallentino@gmail.com>
2026-06-28 20:07:04 +0200

    Mount Bloom route before app shell

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BloomPage from './components/BloomPage.jsx'
+
+const params = new URLSearchParams(window.location.search)
+const Root = params.get('app') === 'bloom' ? BloomPage : App
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
