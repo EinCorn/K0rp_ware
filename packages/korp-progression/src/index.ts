5821c791653b4915489867a1f13f2ae7118e2c59
EinCorn <danielvallentino@gmail.com>
2026-07-11 07:49:47 +0200

    Export K0rp progression database

---
+++ packages/korp-progression/src/index.ts
@@ -0,0 +1,8 @@
+export { KORP_SURFACE_PROGRESSION_DATABASE } from "./surface-progression.database";
+export { KORP_PROGRESSION_DATABASE } from "./progression.database";
+export {
+  assertValidProgressionDatabase,
+  calculateFirstAuditFindings,
+  validateProgressionDatabase
+} from "./progression.validation";
+export type * from "./progression.types";
